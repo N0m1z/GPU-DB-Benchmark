65f51cd1ebf32eeed1aef5340f79580f59427d84
N0m1z <nomis13021@outlook.de>
2021-04-06 16:14:41 +0200

    Initial commit

---
+++ GPU-DB-Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GPU_DB_Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

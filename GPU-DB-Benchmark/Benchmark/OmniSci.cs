bba6c9b44783db5681733d287b2fc11543ce0c6a
N0m1zz <nomis13021@outlook.de>
2021-04-23 16:36:22 +0200

    Finish firts omnisci query

--- GPU-DB-Benchmark/Benchmark/OmniSci.cs
+++ GPU-DB-Benchmark/Benchmark/OmniSci.cs
@@ -7,8 +7,33 @@ namespace GPU_DB_Benchmark.Benchmark
     {
         public void ExecuteQueries()
         {
-            var args = "Queries/OmniSci/Query1.sql | $OMNISCI_PATH/bin/omnisql -p HyperInteractive";
-            Process.Start("cat", args);
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                FileName = "cat",
+                Arguments = "/home/nomis/GPU-DB-Benchmark/GPU-DB-Benchmark/Queries/OmniSci/Query1.sql",
+                RedirectStandardOutput = true
+                }
+            };
+
+            proc.Start();
+            var queryString = proc.StandardOutput.ReadToEnd();
+
+            var omni = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/opt/omnisci/bin/omnisql",
+                    Arguments = "-p HyperInteractive",
+                    RedirectStandardInput = true,
+                }
+            };
+
+            omni.Start();
+            omni.StandardInput.Write(queryString);
+            omni.StandardInput.Close();
+            omni.WaitForExit();
         }
     }
 }

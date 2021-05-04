a03820382d014b27f8d24254f7ae3bbb50781159
Nomis <nomis13021@outlook.de>
2021-05-04 15:50:03 +0200

    Adjust path to csvs

--- GPU-DB-Benchmark/Queries/BlazingSQL/Query2.py
+++ GPU-DB-Benchmark/Queries/BlazingSQL/Query2.py
@@ -2,17 +2,17 @@ from blazingsql import BlazingContext
 bc = BlazingContext()
 
 company_types = ['int64', 'str', 'date', 'str', 'str', 'str']
-bc.create_table('companies', '/home/nomis/500MB_CSV/Companies.csv', dtype=company_types)
+bc.create_table('companies', '/home/nomis/20GB_CSV/Companies.csv', dtype=company_types)
 address_types = ['int64', 'str', 'str', 'str', 'str', 'str', 'str', 'str', 'int64']
-bc.create_table('addresses', '/home/nomis/500MB_CSV/Addresses.csv', dtype=address_types)
+bc.create_table('addresses', '/home/nomis/20GB_CSV/Addresses.csv', dtype=address_types)
 department_types = ['int64', 'str', 'int64']
-bc.create_table('departments', '/home/nomis/500MB_CSV/Departments.csv', dtype=department_types)
+bc.create_table('departments', '/home/nomis/20GB_CSV/Departments.csv', dtype=department_types)
 category_types = ['int64', 'str', 'int64']
-bc.create_table('categories', '/home/nomis/500MB_CSV/Categories.csv', dtype=category_types)
+bc.create_table('categories', '/home/nomis/20GB_CSV/Categories.csv', dtype=category_types)
 product_types = ['int64', 'str', 'str', 'str', 'str', 'date', 'float64', 'int64']
-bc.create_table('products', '/home/nomis/500MB_CSV/Products.csv', dtype=product_types)
+bc.create_table('products', '/home/nomis/20GB_CSV/Products.csv', dtype=product_types)
 review_types = ['int64', 'str', 'str', 'date', 'int64']
-bc.create_table('reviews', '/home/nomis/500MB_CSV/Reviews.csv', dtype=review_types)
+bc.create_table('reviews', '/home/nomis/20GB_CSV/Reviews.csv', dtype=review_types)
 
 query = '''
 SELECT c.Name, a.Country, c.FoundingDate
